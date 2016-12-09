2790c385808eef4fd442f20a659c4506e4ba9a2f
Lesenya <lsmokhubu@gmail.com>
2016-12-09 07:23:27 -0800

    Add files via upload

--- update_task.php
+++ update_task.php
@@ -2,6 +2,9 @@
 /**
  * This script is to be used to receive a POST with the object information and then either updates, creates or deletes the task object
  */
-require('Task.class.php');
-// Assignment: Implement this script
+require('task.class.php');
+$task = new Task();
+$task->TaskName = $_POST['name'];
+$task->TaskDescription = $_POST['description'];
+$task->Save();
 ?>
