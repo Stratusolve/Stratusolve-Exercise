bc4427f6c95a950e3769669fce4fdbd10fbb5c77
Tshehle Mello <tshehle.mello@hellogroup.co.za>
2018-03-05 07:24:54 +0200

    - implemented the jquery and html code on the frontend to display the task information - implemented the jquery code on the frontend that sends the task information to the server - implemented a PHP class that will be able to handle the modification of the task object - implemented a PHP script that receives a POST with the object information and then either updates, creates or deletes the task object - changed the permission on the text data file

--- update_task.php
+++ update_task.php
@@ -2,6 +2,30 @@
 /**
  * This script is to be used to receive a POST with the object information and then either updates, creates or deletes the task object
  */
-require('Task.class.php');
+require( 'task.class.php' );
 // Assignment: Implement this script
-?>
+
+if ( isset( $_POST[ 'action' ] ) )
+{
+
+    $action = $_POST[ 'action' ];
+
+    $task = new Task();
+
+    if ( $action == "save" )
+    {
+        $task->Save( $_POST[ 'name' ], $_POST[ 'description' ], ( int ) $_POST[ 'taskId' ]  );
+    }
+    else if ( $action == "delete" )
+    {
+        $task->Delete( $_POST[ 'id' ] );
+    }
+    else
+    {
+        throw new Exception( "Could not perform the requested operation." );
+    }
+}
+else
+{
+    throw new Exception( "Invalid server request." );
+}
